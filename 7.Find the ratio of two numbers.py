df447e37289e5f333af5fad3119b469360910e4f
cseibm <68462471+cseibm@users.noreply.github.com>
2020-07-18 20:14:06 +0530

    Update 7.Find the ratio of two numbers.py

--- 7.Find the ratio of two numbers.py
+++ 7.Find the ratio of two numbers.py
@@ -1,5 +1,10 @@
-print("enter two nos")
-a= input()
-b= input()
-ratio = int(a)/int(b)
-print ('the ratio of',a,'and',b,'is:',ratio)
+a=input()
+b=input()
+i=1
+while(int(a)>=i and int(b)>=1):
+    if(int(a)%i==0 and int(b)%i==0):
+        GCD=i
+    i=i+1
+n=int(a)/int(GCD)
+m=int(b)/int(GCD)
+print("ratio is",n,":",m)
