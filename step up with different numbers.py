d915cb6a832f40bd6a9007f786e95c520bc99fba
cseibm <68462471+cseibm@users.noreply.github.com>
2020-07-28 00:22:04 +0530

    Create step up with different numbers.py

---
+++ step up with different numbers.py
@@ -0,0 +1,18 @@
+a)
+# step up with different numbers
+a=0
+n = int(input("Enter nth Number: "))
+x=int(input("enter the step up number:"))
+for i in range(a,n):
+    a=a+x
+    a=a+1
+    print (a+1, end = "  ")
+
+b)
+# multiples of numbers
+a=0
+n = int(input("Enter nth Number: "))
+x=int(input("enter the step up number:"))
+for i in range(a,n):
+    a=a+x
+    print (a, end = "  ")
